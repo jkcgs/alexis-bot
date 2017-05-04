e9803d31ceb2dd103a854f5e0171f437a9cd3793
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-05-04 01:02:33 +0000

    Ordenar código

--- alexis.py
+++ alexis.py
@@ -46,30 +46,30 @@ if __name__ == '__main__':
                 for subreddit in config['subreddit']:
                     r = requests.get('https://www.reddit.com/r/{}/new/.json'.format(subreddit),
                                      headers={'User-agent': 'Alexis'})
-                    if r.status_code == 200:
-                        try:
-                            exists = Post.select().where(Post.id == r.json()['data']['children'][0]['data']['id']).get()
-                        except:
-                            exists = False
+                    if not r.status_code == 200:
+                        continue
 
-                        while r.json()['data']['children'][0]['data']['id'] != post_id and not exists:
-                            j = r.json()['data']['children'][0]['data']
-                            post_id = j['id']
+                    data = r.json()['data']['children'][0]['data']
 
-                            if j['over_18']:
-                                channels = config['channel_nsfw']
-                            else:
-                                channels = config['channel_id']
+                    try:
+                        exists = Post.select().where(Post.id == data['id']).get()
+                    except:
+                        exists = False
 
-                            for channel in channels:
-                                await client.send_message(discord.Object(id=channel),
-                                                          'Nuevo post en **/r/{subreddit}** por **/u/{autor}**: https://www.reddit.com{permalink}'.format(
-                                                              subreddit=j['subreddit'], autor=j['author'],
-                                                              permalink=j['permalink']))
+                    while data['id'] != post_id and not exists:
+                        post_id = data['id']
+                        channels = config['channel_nsfw'] if data['over_18'] else config['channel_id']
 
-                            if not exists:
-                                Post.create(id=post_id, permalink=j['permalink'], over_18=j['over_18'])
-                                log.info('Nuevo post en /r/{}: {}'.format(j['subreddit'], j['permalink']))
+                        for channel in channels:
+                            d = 'Nuevo post en **/r/{subreddit}** por **/u/{autor}**: https://www.reddit.com{permalink}'
+                            text = d.format(subreddit=data['subreddit'],
+                                            autor=data['author'],
+                                            permalink=data['permalink'])
+                            await client.send_message(discord.Object(id=channel), text)
+
+                        if not exists:
+                            Post.create(id=post_id, permalink=data['permalink'], over_18=data['over_18'])
+                            log.info('Nuevo post en /r/{}: {}'.format(data['subreddit'], data['permalink']))
             except Exception as e:
                 log.warning(e)
             await asyncio.sleep(60)
@@ -80,15 +80,17 @@ if __name__ == '__main__':
         if message.content == '!ping':
             await client.send_message(message.channel, 'pong!')
         elif message.content.startswith('!ban'):
-            if message.server:
-                mentions = message.mentions
-                for mention in mentions:
-                    user, created = Ban.get_or_create(user=mention, server=message.server)
-                    up = Ban.update(bans=Ban.bans + 1).where(Ban.user == mention, Ban.server == message.server)
-                    up.execute()
-                    await client.send_message(message.channel,
-                                              'El usuario **{}** ha sido baneado {} veces.'.format(mention.name,
-                                                                                                   user.bans + 1))
+            if message.is_private:
+                return
+
+            mentions = message.mentions
+            for mention in mentions:
+                user, created = Ban.get_or_create(user=mention, server=message.server)
+                up = Ban.update(bans=Ban.bans + 1).where(Ban.user == mention, Ban.server == message.server)
+                up.execute()
+
+                text = 'El usuario **{}** ha sido baneado {} veces.'.format(mention.name, user.bans + 1)
+                await client.send_message(message.channel, text)
 
 
     @client.event
