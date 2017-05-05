8c961199c60181b50022fa37f833fcebe136c522
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-05-05 01:52:01 -0300

    AGENT

--- subreddit.py
+++ subreddit.py
@@ -3,7 +3,7 @@ import requests
 
 def get_posts(sub, since=0):
     url = 'https://www.reddit.com/r/{}/new/.json'.format(sub)
-    r = requests.get(url, headers={'User-agent': 'Agent'})
+    r = requests.get(url, headers={'User-agent': 'Alexis'})
 
     if not r.status_code == 200:
         return []
