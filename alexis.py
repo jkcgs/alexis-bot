21508085bfa6ab88f6c84929718eaca8907d580d
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-06-27 06:14:07 -0400

    Subir esta cuenta por fails

--- alexis.py
+++ alexis.py
@@ -16,7 +16,7 @@ from tasks import posts_loop
 
 __author__ = 'Nicolás Santisteban, Jonathan Gutiérrez'
 __license__ = 'MIT'
-__version__ = '0.1.3-memes.1'
+__version__ = '0.1.3-memes.3'
 __status__ = "Desarrollo"
 
 
