7f3a7a104dd21959c90e173b7a5d740952330b5d
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-07-07 02:57:09 -0400

    Cambiar versión para integrar a dev

--- alexis.py
+++ alexis.py
@@ -16,7 +16,7 @@ from tasks import posts_loop
 
 __author__ = 'Nicolás Santisteban, Jonathan Gutiérrez'
 __license__ = 'MIT'
-__version__ = '0.1.3-memes.5'
+__version__ = '0.1.4-dev'
 __status__ = "Desarrollo"
 
 
