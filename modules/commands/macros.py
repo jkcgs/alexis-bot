4c237676ff9bf815011e0a16156c1e0ac55dab99
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-10-22 18:31:09 -0300

    Quitar "default_macros"

--- modules/commands/macros.py
+++ modules/commands/macros.py
@@ -141,19 +141,8 @@ class MacroUse(Command):
         super().__init__(bot)
         self.swhandler = [bot.config['command_prefix'] + ' ', '¡']
         self.first_use = True
-        self.default_macros = self.load_config()
 
     async def handle(self, message, cmd):
-        # Inicializar macros por defecto
-        if self.first_use:
-            num_memes = len(self.default_macros)
-            if num_memes > 0:
-                self.log.info('Inicializando base de datos...')
-                for meme_name, meme_cont in self.default_macros.items():
-                    Meme.get_or_create(name=meme_name, content=meme_cont)
-                self.log.info('Base de datos inicializada')
-            self.first_use = False
-
         # Actualizar el id de la última persona que usó el comando, omitiendo al mismo bot
         if self.bot.last_author is None or not cmd.own:
             self.bot.last_author = message.author.id
@@ -186,17 +175,6 @@ class MacroUse(Command):
             return
         except Meme.DoesNotExist:
             pass
-
-    def load_config(self):
-        try:
-            config_path = path.join(path.dirname(path.realpath(__file__)), 'macros.yml')
-            with open(config_path, 'r') as file:
-                config = yaml.safe_load(file)
-
-            return {} if 'default_macros' not in config else config['default_macros']
-        except Exception as ex:
-            self.log.exception(ex)
-            return {}
 
 
 class EmbedMacroSet(Command):
