6874926ede3ab666022be45305fae9e46e04ae48
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-10-27 00:48:29 -0300

    Versión completa de lmgtfy

--- modules/commands/lmgtfy.py
+++ modules/commands/lmgtfy.py
@@ -14,6 +14,6 @@ class LetMeGoogleThatForYou(Command):
             return
 
         texto = ' '.join(cmd.args)
-        url = 'https://lmgtfy.com/?' + urlencode({'q': texto})
+        url = 'https://lmgtfy.com/?' + urlencode({'iie': texto})
 
         await cmd.answer(url)
