fd82b2f193ac70f80d1984c378c3b36f39568131
Jonathan Gutierrez <contacto@jgutierrez.cl>
2017-10-29 02:15:56 -0300

    !clearreactions (!clr para los amigos)

--- modules/commands/owner_cmds.py
+++ modules/commands/owner_cmds.py
@@ -1,3 +1,7 @@
+import re
+
+import discord
+
 from modules.base.command import Command
 from discord import Game
 import sys
@@ -56,3 +60,48 @@ class SetStatus(Command):
         status = '' if len(cmd.args) < 1 else cmd.text
         await self.bot.change_presence(game=Game(name=status))
         await cmd.answer('k')
+
+
+class ClearReactions(Command):
+    rx_snowflake = re.compile('^\d{10,19}$')
+    rx_channel = re.compile('^<#\d{10,19}>$')
+
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.name = ['clearreactions', 'clr']
+        self.help = 'Elimina las reacciones de uno o más mensajes'
+        self.owner_only = True
+
+    async def handle(self, message, cmd):
+        if cmd.argc < 1:
+            prefix = self.bot.config['command_prefix']
+            await cmd.answer('Formato: {}{} [#canal=actual] <id_mensaje1> ... <id_mensajeN>'.format(prefix, cmd.cmdname))
+            return
+
+        channel = message.channel
+        if ClearReactions.rx_channel.match(cmd.args[0]):
+            channel = message.cha374061207000449024nnel_mentions[0]
+            cmd.args = cmd.args[1:]
+            cmd.argc -= 1
+
+        filtered_len = len([f for f in cmd.args if ClearReactions.rx_snowflake.match(f)])
+        if cmd.argc != filtered_len:
+            await cmd.answer('Por favor ingresa formatos de IDs compatibles')
+            return
+
+        success_count = 0
+        for arg in cmd.args:
+            try:
+                msg = await self.bot.get_message(channel, arg)
+                await self.bot.clear_reactions(msg)
+                success_count += 1
+            except discord.Forbidden:
+                pass
+
+        if success_count == 0:
+            msg_suffix = 'del mensaje' if cmd.argc == 1 else 'de ningún mensaje'
+            await cmd.answer('No se pudo limpiar las reacciones ' + msg_suffix)
+        elif success_count < cmd.argc:
+            await cmd.answer('Se eliminaron las reacciones de algunos mensajes')
+        else:
+            await cmd.answer('Reacciones eliminadas correctamente')
