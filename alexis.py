75fb08d73fe011fa6697ae1d315ca6146673599c
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-11-06 01:38:10 -0300

    Refractor: mover message_handler a Command y reutilizar código

--- alexis.py
+++ alexis.py
@@ -13,16 +13,14 @@ import discord
 import peewee
 import yaml
 
-from datetime import datetime as dt
-from datetime import timedelta
-
 import modules.commands
 from modules import logger
+from modules.base.command import Command
 from modules.reaction_hook import reaction_hook
 
 __author__ = 'Nicolás Santisteban, Jonathan Gutiérrez'
 __license__ = 'MIT'
-__version__ = '0.7.0-dev'
+__version__ = '0.8.0-dev'
 __status__ = "Desarrollo"
 
 
@@ -138,90 +136,30 @@ class Alexis(discord.Client):
         if not self.initialized:
             return
 
-        # Info sobre el mensaje
-        text = message.content
-        author = message.author.display_name
-        chan = message.channel
-        is_pm = message.server is None
-        is_owner = self.is_owner(message.author, message.server)
-        author_id = message.author.id
-        own_message = author_id == self.user.id
-
-        # Mandar PMs al log
-        if is_pm:
-            if own_message:
-                self.log.info('[PM] (-> %s): %s', message.channel.user, text)
-            else:
-                self.log.info('[PM] %s: %s', author, text)
-
-        # Command handler
-        if text.startswith(self.config['command_prefix']) and len(text) > 1:
-            try:
-                cmd = text.split(' ')[0][1:]
-                if cmd in self.cmds:
-                    # Si es posible, revisar que el canal no ha sido bloqueado
-                    if not is_pm and not is_owner and 'lockbot' in self.cmds:
-                        lbinstance = self.cmds['lockbot'][0]
-                        lbname = lbinstance.name
-                        lbnames = [lbname] if isinstance(lbname, str) else lbname
-                        if cmd not in lbname and lbinstance.is_locked(message.server.id, chan.id):
-                            return
-
-                    self.log.debug('[command] %s sent message: "%s" command %s', message.author, text, cmd)
-                    for i in self.cmds[cmd]:
-                        if i.owner_only and not is_owner:
-                            await self.send_message(chan, i.owner_error)
-                        elif not i.allow_pm and is_pm:
-                            await self.send_message(chan, i.pm_error)
-                        elif i.user_delay > 0 and author_id in i.users_delay \
-                                and i.users_delay[author_id] + timedelta(0, i.user_delay) > dt.now() \
-                                and not is_owner:
-                            await self.send_message(chan, i.user_delay_error)
-                        else:
-                            i.users_delay[author_id] = dt.now()
-                            await i.handle(message, i.parse(message))
-                    return
-            except Exception as e:
-                await self.send_message(chan, 'ocurr.. 1.error c0n\'el$##com@nd..\n```{}```'.format(str(e)))
-                self.log.exception(e)
-
-        # 'startswith' handlers
-        swbreak = False
-        for swtext in self.swhandlers.keys():
-            if swbreak:
-                break
-
-            if message.content.startswith(swtext):
-                self.log.debug('[sw] %s sent message: "%s" handler "%s"', message.author, text, swtext)
-                for cmd in self.swhandlers[swtext]:
-                    if cmd.owner_only and not is_owner:
-                        await self.send_message(chan, cmd.owner_error)
-                    elif not cmd.allow_pm and is_pm:
-                        await self.send_message(chan, cmd.pm_error)
-                    else:
-                        await cmd.handle(message, cmd.parse(message))
-
-                    if cmd.swhandler_break:
-                        swbreak = True
-                        break
-
-        # Mention handlers
-        if self.user.mentioned_in(message):
-            for i in self.mention_handlers:
-                if i.owner_only and not is_owner:
-                    await self.send_message(chan, i.owner_error)
-                elif not i.allow_pm and is_pm:
-                    await self.send_message(chan, i.pm_error)
-                else:
-                    await i.handle(message, i.parse(message))
+        await Command.message_handler(message, self)
 
     """Esta función es llamada cuando un mensaje recibe una reacción"""
     async def on_reaction_add(self, reaction, user):
+        if not self.initialized:
+            return
+
         await reaction_hook(self, reaction, user)
 
     async def on_member_join(self, member):
+        if not self.initialized:
+            return
+
         for cmd in self.cmd_instances:
             await cmd.on_member_join(member)
+
+    async def send_message(self, destination, content=None, **kwargs):
+        svid = destination.server.id if isinstance(destination, discord.Channel) else 'PM?'
+        msg = 'Sending message "{}" to {}#{}'.format(content, svid, destination)
+        if isinstance(kwargs.get('embed'), discord.Embed):
+            msg += ' (with embed: {})'.format(kwargs.get('embed').to_dict())
+
+        self.log.debug(msg)
+        await super(Alexis, self).send_message(destination, content, **kwargs)
 
     def load_config(self):
         try:
@@ -241,29 +179,6 @@ class Alexis(discord.Client):
         except Exception as ex:
             self.log.exception(ex)
             return False
-
-    def is_owner(self, member, server):
-        if server is None:
-            return False
-
-        if member.id in self.config['owners']:
-            return True
-
-        for role in member.roles:
-            owner_role = server.id + "@" + role.id
-            if owner_role in self.config['owners']:
-                return True
-
-        return False
-
-    async def send_message(self, destination, content=None, **kwargs):
-        svid = destination.server.id if isinstance(destination, discord.Channel) else 'PM?'
-        msg = 'Sending message "{}" to {}#{}'.format(content, svid, destination)
-        if isinstance(kwargs.get('embed'), discord.Embed):
-            msg += ' (with embed: {})'.format(kwargs.get('embed').to_dict())
-
-        self.log.debug(msg)
-        await super(Alexis, self).send_message(destination, content, **kwargs)
 
 
 if __name__ == '__main__':
--- modules/base/command.py
+++ modules/base/command.py
@@ -2,6 +2,9 @@ from discord import Embed
 
 from modules.base.database import ServerConfig
 
+from datetime import datetime as dt
+from datetime import timedelta
+
 
 class Command:
     def __init__(self, bot):
@@ -25,25 +28,6 @@ class Command:
 
         self.db_models = []
         self.http = bot.http_session
-
-    def parse(self, message):
-        msg = Message(message, self.bot)
-        msg.owner = self.is_owner(message.author, message.server)
-        return msg
-
-    def is_owner(self, member, server):
-        if server is None:
-            return False
-
-        if member.id in self.bot.config['owners']:
-            return True
-
-        for role in member.roles:
-            owner_role = server.id + "@" + role.id
-            if owner_role in self.bot.config['owners']:
-                return True
-
-        return False
 
     async def on_member_join(self, member):
         pass
@@ -86,6 +70,9 @@ class Command:
         conf.value = value
         conf.save()
 
+    def is_owner(self, member, server):
+        return Command.is_owner(self.bot, member, server)
+
     @staticmethod
     def img_embed(url, title=''):
         embed = Embed()
@@ -101,8 +88,101 @@ class Command:
                 return role
         return None
 
+    @staticmethod
+    def parse(message, bot):
+        msg = MessageCmd(message, bot)
+        msg.owner = Command.is_owner(bot, message.author, message.server)
+        return msg
 
-class Message:
+    @staticmethod
+    def is_owner(bot, member, server):
+        if server is None:
+            return False
+
+        if member.id in bot.config['owners']:
+            return True
+
+        for role in member.roles:
+            owner_role = server.id + "@" + role.id
+            if owner_role in bot.config['owners']:
+                return True
+
+        return False
+
+    @staticmethod
+    async def message_handler(message, bot):
+        cmd = Command.parse(message, bot)
+
+        # Mandar PMs al log
+        if cmd.is_pm:
+            if cmd.own:
+                bot.log.info('[PM] (-> %s): %s', message.channel.user, cmd.text)
+            else:
+                bot.log.info('[PM] %s: %s', cmd.author, cmd.text)
+
+        # Si es posible, revisar que el canal no ha sido bloqueado (no se revisa si es un PM, si es owner, o si es
+        # el comando !lock o lockbot)
+        if not cmd.is_pm and not cmd.owner and 'lockbot' in bot.cmds:
+            lbinstance = bot.cmds['lockbot'][0]
+            lbname = lbinstance.name
+            lbnames = [lbname] if isinstance(lbname, str) else lbname
+            is_lb_cmd = cmd.is_cmd and cmd.cmdname not in lbnames
+            if not is_lb_cmd and lbinstance.is_locked(message.server.id, message.channel.id):
+                return
+
+        # Command handler
+        try:
+            if cmd.is_cmd and cmd.cmdname in bot.cmds:
+                bot.log.debug('[command] %s: %s', cmd.author, str(cmd))
+                for i in bot.cmds[cmd.cmdname]:
+                    if i.owner_only and not cmd.owner:
+                        await cmd.answer(i.owner_error)
+                    elif not i.allow_pm and cmd.is_pm:
+                        await cmd.answer(i.pm_error)
+                    elif i.user_delay > 0 and cmd.author.id in i.users_delay \
+                            and i.users_delay[cmd.author.id] + timedelta(0, i.user_delay) > dt.now() \
+                            and not cmd.owner:
+                        await cmd.answer(i.user_delay_error)
+                    else:
+                        i.users_delay[cmd.author.id] = dt.now()
+                        await i.handle(message, cmd)
+                return
+        except Exception as e:
+            await cmd.answer('ocurr.. 1.error c0n\'el$##com@nd..\n```{}```'.format(str(e)))
+            bot.log.exception(e)
+
+        # 'startswith' handlers
+        swbreak = False
+        for swtext in bot.swhandlers.keys():
+            if swbreak:
+                break
+
+            if message.content.startswith(swtext):
+                bot.log.debug('[sw] %s sent message: "%s" handler "%s"', message.author, cmd.text, swtext)
+                for swhandler in bot.swhandlers[swtext]:
+                    if swhandler.owner_only and not cmd.owner:
+                        await cmd.answer(swhandler.owner_error)
+                    elif not swhandler.allow_pm and cmd.is_pm:
+                        await cmd.answer(swhandler.pm_error)
+                    else:
+                        await swhandler.handle(message, cmd)
+
+                    if swhandler.swhandler_break:
+                        swbreak = True
+                        break
+
+        # Mention handlers
+        if bot.user.mentioned_in(message):
+            for i in bot.mention_handlers:
+                if i.owner_only and not cmd.owner:
+                    await cmd.answer(i.owner_error)
+                elif not i.allow_pm and cmd.is_pm:
+                    await cmd.answer(i.pm_error)
+                else:
+                    await i.handle(message, cmd)
+
+
+class MessageCmd:
     def __init__(self, message, bot):
         self.bot = bot
         self.message = message
@@ -112,12 +192,20 @@ class Message:
         self.own = message.author.id == bot.user.id
         self.owner = False
         self.server_member = None
+        self.is_cmd = False
+        self.text = message.content
 
-        allargs = message.content.replace('  ', ' ').split(' ')
-        self.args = [] if len(allargs) == 1 else [f for f in allargs[1:] if f.strip() != '']
-        self.argc = len(self.args)
-        self.cmdname = allargs[0][1:]
-        self.text = ' '.join(self.args)
+        self.cmdname = ''
+        self.args = []
+        self.argc = 0
+
+        if message.content.startswith(bot.config['command_prefix']):
+            self.is_cmd = True
+            allargs = message.content.replace('  ', ' ').split(' ')
+            self.args = [] if len(allargs) == 1 else [f for f in allargs[1:] if f.strip() != '']
+            self.argc = len(self.args)
+            self.cmdname = allargs[0][1:]
+            self.text = ' '.join(self.args)
 
         if not self.is_pm:
             self.server_member = message.server.get_member(self.bot.user.id)
@@ -147,6 +235,10 @@ class Message:
                 return channel
 
         return None
+
+    def __str__(self):
+        return '[MessageCmd name="{}", channel="{}#{}" text="{}"]'.format(
+            self.cmdname, self.message.server, self.message.channel, self.text)
 
 
 class ConfigError(BaseException):
