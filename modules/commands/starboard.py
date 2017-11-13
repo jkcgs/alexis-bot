25b4914e37ca4ad2e0b6e305555202cad229b861
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-11-13 01:17:37 -0300

    Unir comandos de starboard en uno

--- modules/commands/starboard.py
+++ modules/commands/starboard.py
@@ -8,6 +8,7 @@ from discord import Emoji, Object, Embed
 from modules.base.command import Command
 from modules.base.database import BaseModel, ServerConfig
 
+default_count = 10
 cfg_starboard_emojis = 'starboard_emojis'
 cfg_starboard_channel = 'starboard_channel'
 cfg_starboard_tcount = 'starboard_trigger_count'
@@ -18,6 +19,97 @@ class StarboardHook(Command):
     def __init__(self, bot):
         super().__init__(bot)
         self.db_models = [Starboard]
+        self.allow_pm = False
+        self.owner_only = True
+        self.name = 'starboard'
+
+    async def handle(self, message, cmd):
+        args = [] if cmd.argc == 0 else cmd.args[1:]
+        argc = len(args)
+        subcmd = None if cmd.argc == 0 else cmd.args[0]
+
+        if subcmd == 'channel':
+            channel = None
+            if argc == 0:
+                channel = message.channel
+            elif argc == 1:
+                if len(message.channel_mentions) > 0:
+                    channel = message.channel_mentions[0]
+                else:
+                    channel = cmd.find_channel(args[0])
+
+            if channel is None:
+                await cmd.answer('Canal no encontrado')
+                return
+
+            config, _ = ServerConfig.get_or_create(serverid=message.server.id, name=cfg_starboard_channel)
+            config.value = channel.id
+            config.save()
+
+            await cmd.answer('Canal de starboard actualizado a <#{}>!'.format(channel.id))
+        elif subcmd == 'disable':
+            config, _ = ServerConfig.get_or_create(serverid=message.server.id, name=cfg_starboard_channel)
+            config.value = ''
+            config.save()
+
+            await cmd.answer('Starboard desactivado! Para volver a activarlo, utiliza el subcomando "channel".')
+        elif subcmd == 'count':
+            config, _ = ServerConfig.get_or_create(defaults={'value': str(default_count)},
+                                                   serverid=message.server.id, name=cfg_starboard_tcount)
+            if argc == 0:
+                await cmd.answer('Cuenta trigger actual: ' + str(config.value))
+                return
+
+            if not args[0].isdigit():
+                await cmd.answer('Utiliza un número entero entre 1 y 10, inclusive.')
+                return
+
+            count = int(args[0])
+            if count < 1 or count > 1000:
+                await cmd.answer('Utiliza un número entero entre 1 y 10, inclusive.')
+                return
+
+            config.value = str(count)
+            config.save()
+
+            await cmd.answer('Número de trigger definido en **{}**'.format(count))
+        elif subcmd == 'emojis':
+            config, _ = ServerConfig.get_or_create(defaults={'value': ''},
+                                                   serverid=message.server.id, name=cfg_starboard_emojis)
+
+            if argc == 0:
+                val = 'Ninguno' if config.value == '' else config.value
+                await cmd.answer('Emojis: ' + val)
+                return
+
+            if argc > 10:
+                await cmd.answer('Formato: <inserte formato aquí>')
+                return
+
+            for arg in cmd.args:
+                if not pat_emoji.match(arg) and arg not in emoji.UNICODE_EMOJI:
+                    await cmd.answer('Formato: <inserte formato aquí>')
+                    return
+
+            config, _ = ServerConfig.get_or_create(defaults={'value': ''},
+                                                   serverid=message.server.id, name=cfg_starboard_emojis)
+            config.value = ' '.join(cmd.text)
+            config.save()
+
+            await cmd.answer('Emojis guardados: ' + cmd.text)
+        elif subcmd == 'delemojis':
+            config, _ = ServerConfig.get_or_create(defaults={'value': ''},
+                                                   serverid=message.server.id, name=cfg_starboard_emojis)
+            if config.value == '':
+                await cmd.answer('No hay emojis definidos para este servidor')
+                return
+
+            config.value = ''
+            config.save()
+            await cmd.answer('Emojis de starboard eliminados (ahora se reacciona con cualquiera).')
+        else:
+            await cmd.answer('Formato: !{} <subcomando> <opts>.\n'
+                             'Subcomandos: channel, disable, count, emojis, delemojis.'.format(cmd.cmdname))
 
     async def on_reaction_add(self, reaction, user):
         msg = reaction.message
@@ -45,12 +137,12 @@ class StarboardHook(Command):
             return
         channelid = config.value
 
-        ct_config, _ = ServerConfig.get_or_create(defaults={'value': str(StarboardTriggerCount.default)},
+        ct_config, _ = ServerConfig.get_or_create(defaults={'value': str(default_count)},
                                                   serverid=reaction.message.server.id,
                                                   name=cfg_starboard_tcount)
         if not ct_config.value.isdigit():
-            count_trigger = StarboardTriggerCount.default
-            ct_config.value = str(StarboardTriggerCount.default)
+            count_trigger = default_count
+            ct_config.value = str(default_count)
             ct_config.save()
         else:
             count_trigger = int(ct_config.value)
@@ -99,139 +191,6 @@ class StarboardHook(Command):
         embed.add_field(name='Reacciones', value=reactions)
 
         await self.bot.send_message(channel, embed=embed)
-
-
-class StarboardChanSet(Command):
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.name = ['setstarboardchannel', 'ssc']
-        self.owner_only = True
-        self.allow_pm = False
-
-    async def handle(self, message, cmd):
-        channel = None
-        if cmd.argc == 0:
-            channel = message.channel
-        elif cmd.argc == 1:
-            if len(message.channel_mentions) > 0:
-                channel = message.channel_mentions[0]
-            else:
-                channel = cmd.find_channel(cmd.args[0])
-
-        if channel is None:
-            await cmd.answer('Canal no encontrado')
-            return
-
-        config, _ = ServerConfig.get_or_create(serverid=message.server.id, name=cfg_starboard_channel)
-        config.value = channel.id
-        config.save()
-
-        await cmd.answer('Canal de starboard actualizado a <#{}>!'.format(channel.id))
-
-
-class StarboardChanUnset(Command):
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.name = ['usetstarboardchannel', 'usc', 'disablestarboard']
-        self.owner_only = True
-        self.allow_pm = False
-
-    async def handle(self, message, cmd):
-        config, _ = ServerConfig.get_or_create(serverid=message.server.id, name=cfg_starboard_channel)
-        config.value = ''
-        config.save()
-
-        await cmd.answer('Starboard desactivado!')
-
-
-class StarboardTriggerCount(Command):
-    default = 10
-
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.name = ['setstarboardtriggercount', 'starboardcount', 'stc']
-        self.owner_only = True
-        self.allow_pm = False
-        self.format = 'Formato: !{} <cuenta (0>N>=1000)>'
-
-    async def handle(self, message, cmd):
-        config, _ = ServerConfig.get_or_create(defaults={'value': str(StarboardTriggerCount.default)},
-                                               serverid=message.server.id, name=cfg_starboard_tcount)
-        if cmd.argc == 0:
-            await cmd.answer('Cuenta trigger actual: ' + str(config.value))
-            return
-
-        if not cmd.args[0].isdigit():
-            await cmd.answer(self.format.format(cmd.cmdname))
-            return
-
-        count = int(cmd.args[0])
-        if count < 1 or count > 1000:
-            await cmd.answer(self.format.format(cmd.cmdname))
-            return
-
-        config.value = str(count)
-        config.save()
-
-        await cmd.answer('Número de trigger definido en **{}**'.format(count))
-
-
-class StarboardSetEmojis(Command):
-
-    default = 10
-
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.name = ['setstarboardemojis', 'sse', 'setemojis']
-        self.owner_only = True
-        self.allow_pm = False
-        self.format = 'Formato: !{} <emoji1 emoji2...emoji10>'
-
-    async def handle(self, message, cmd):
-        config, _ = ServerConfig.get_or_create(defaults={'value': ''},
-                                               serverid=message.server.id, name=cfg_starboard_emojis)
-
-        if cmd.argc == 0:
-            val = 'Ninguno' if config.value == '' else config.value
-            await cmd.answer('Emojis: ' + val)
-            return
-
-        if cmd.argc > 10:
-            await cmd.answer(self.format.format(cmd.cmdname))
-            return
-
-        for arg in cmd.args:
-            if not pat_emoji.match(arg) and arg not in emoji.UNICODE_EMOJI:
-                await cmd.answer(self.format.format(cmd.cmdname))
-                return
-
-        config, _ = ServerConfig.get_or_create(defaults={'value': ''},
-                                               serverid=message.server.id, name=cfg_starboard_emojis)
-        config.value = ' '.join(cmd.text)
-        config.save()
-
-        await cmd.answer('Emojis guardados: ' + cmd.text)
-
-
-class StarboardUnsetEmojis(Command):
-    default = 10
-
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.name = ['unsetstarboardemojis', 'unsetemojis']
-        self.owner_only = True
-        self.allow_pm = False
-
-    async def handle(self, message, cmd):
-        config, _ = ServerConfig.get_or_create(defaults={'value': ''},
-                                               serverid=message.server.id, name=cfg_starboard_emojis)
-        if config.value == '':
-            await cmd.answer('No hay emojis definidos para este servidor')
-            return
-
-        config.value = ''
-        config.save()
-        await cmd.answer('Emojis de starboard eliminados (ahora se reacciona con cualquiera).')
 
 
 class Starboard(BaseModel):
