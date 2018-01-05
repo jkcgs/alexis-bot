ba8c1cc5dc7cf0708a46b864755eea093ccf2b6a
Jonathan Gutierrez <contacto@jgutierrez.cl>
2018-01-05 18:55:22 -0300

    Listar canales bloqueados

--- alexis/modules/lockbot.py
+++ alexis/modules/lockbot.py
@@ -1,5 +1,10 @@
+from discord import Embed
+
 from alexis import Command
 from alexis.base.database import ServerConfig
+
+
+cfg_locked = 'locked_bot_channels'
 
 
 class LockBot(Command):
@@ -35,7 +40,7 @@ class LockBot(Command):
             await cmd.answer('canal no encontrado')
             return
 
-        chans = cmd.config.get_list('locked_bot_channels',)
+        chans = cmd.config.get_list(cfg_locked)
         chanid = 'all' if chall else channel.id
 
         if cmd.cmdname == 'islocked':
@@ -54,7 +59,7 @@ class LockBot(Command):
                     await cmd.answer('ya está todo bloqueado')
                     return
                 else:
-                    cmd.config.add('locked_bot_channels', 'all')
+                    cmd.config.add(cfg_locked, 'all')
                     await cmd.answer('ahora todo está bloqueado ewe')
                     return
             else:
@@ -63,11 +68,11 @@ class LockBot(Command):
                     return
                 else:
                     if cmd.argc > 1 and cmd.args[1] == 'keep':
-                        cmd.config.remove('locked_bot_channels', 'all')
+                        cmd.config.remove(cfg_locked, 'all')
                         await cmd.answer('desbloqueado todo (excepto lo que estaba bloqueado antes del bloqueo total)')
                         return
                     else:
-                        cmd.config.set('locked_bot_channels', '')
+                        cmd.config.set(cfg_locked, '')
                         await cmd.answer('desbloqueado todo')
                         return
 
@@ -76,18 +81,55 @@ class LockBot(Command):
                 await cmd.answer('el canal ya está bloqueado, usa $PXunlockbot para desbloquear.')
                 return
 
-            cmd.config.remove('locked_bot_channels', chanid)
+            cmd.config.remove(cfg_locked, chanid)
             await cmd.answer('canal desbloqueado! :smile:')
         else:
             if not lock:
                 await cmd.answer('el canal no está bloqueado, usa $PXlockbot para bloquear.')
                 return
 
-            cmd.config.add('locked_bot_channels', chanid)
+            cmd.config.add(cfg_locked, chanid)
             await cmd.answer('canal bloqueado jajaj ewe')
 
 
+class LockedChans(Command):
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.name = 'lockedlist'
+        self.owner_only = True
+        self.allow_pm = False
+
+    async def handle(self, message, cmd):
+        chans = cmd.config.get_list(cfg_locked)
+        is_all = 'all' in chans
+        others = [f for f in chans if f != 'all']
+        chan_list = []
+        for chanid in others:
+            chan = message.server.get_channel(chanid)
+            if chan is None:
+                chan_list.append('- {} (no encontrado)'.format(chanid))
+            else:
+                chan_list.append('- {} (ID: {})'.format(chan.mention, chanid))
+
+        msg = ''
+        if is_all:
+            msg = 'todos los canales están bloqueados.'
+            if len(others) > 0:
+                msg += ' Además, los siguientes canales han sido marcados como bloqueados.'
+        else:
+            if len(others) > 0:
+                msg += 'los siguientes canales están bloqueados'
+            else:
+                msg = 'no hay canales bloqueados.'
+
+        embed = None
+        if len(others) > 0:
+            embed = Embed(description='\n'.join(chan_list))
+
+        await cmd.answer(msg, embed=embed)
+
+
 def is_locked(serverid, channelid):
-    config, _ = ServerConfig.get_or_create(serverid=serverid, name='locked_bot_channels')
+    config, _ = ServerConfig.get_or_create(serverid=serverid, name=cfg_locked)
     val = [] if config.value == '' else config.value.split(',')
     return 'all' in val or channelid in val
