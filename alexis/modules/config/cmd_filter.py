a09123e387fceddd30912ff50f86cea17d257748
Jonathan Gutierrez <contacto@jgutierrez.cl>
2018-01-31 11:35:38 -0300

    Fix #68

---
+++ alexis/modules/config/cmd_filter.py
@@ -0,0 +1,21 @@
+from alexis import Command
+from alexis.database import ServerConfigMgrSingle
+
+
+class CmdFilter(Command):
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    def pre_send_message(self, kwargs):
+        dest = kwargs.get('destination')
+        svid = dest.server.id if hasattr(dest, 'server') else None
+        if svid is None:
+            return
+
+        svconfig = ServerConfigMgrSingle(self.bot.sv_config, svid)
+        prefix = svconfig.get('command_prefix', self.bot.config['command_prefix'])
+        if prefix == '':
+            return
+
+        if kwargs.get('content', '').startswith(prefix):
+            kwargs['content'] = kwargs['content'][len(prefix):]
