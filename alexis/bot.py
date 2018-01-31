fa92f6502cd3c888086371f91464ed6d7b830c45
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-01-30 22:04:11 -0300

    Agregar alias para !clearwarns, !unwarn

--- alexis/bot.py
+++ alexis/bot.py
@@ -23,7 +23,7 @@ from alexis.database import db
 class Alexis(discord.Client):
     __author__ = 'ibk (github.com/santisteban), makzk (github.com/jkcgs)'
     __license__ = 'MIT'
-    __version__ = '1.0.0-dev.27~f1'
+    __version__ = '1.0.0-dev.27~f2'
 
     default_config = {
         'token': '',
