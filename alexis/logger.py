600a6853cc7411f58dece0239820e54e368c8b3b
Jonathan Gutierrez <contacto@jgutierrez.cl>
2018-02-02 15:26:25 -0300

    Log a archivo y ruta de logs configurable Ahora es posible desactivar guardar registros a archivo

--- alexis/logger.py
+++ alexis/logger.py
@@ -2,6 +2,10 @@ import logging
 import os
 import datetime
 
+from os import path
+
+from alexis.configuration import StaticConfig
+
 logger_created = False
 
 
@@ -10,24 +14,29 @@ def get_logger(name):
     logger = logging.getLogger(name)
 
     if not logger_created:
+        config = StaticConfig('config.yml')
+        config.load({'log_path': 'logs', 'log_to_files': True})
+
         logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(message)s', '%d-%m-%Y %H:%M:%S')
+        formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(message)s', '%Y-%m-%d %H:%M:%S')
         stdout_logger = logging.StreamHandler()
         stdout_logger.setLevel(logging.DEBUG)
         stdout_logger.setFormatter(formatter)
         logger.addHandler(stdout_logger)
 
-        try:
-            if not os.path.isdir('logs/'):
-                os.makedirs('logs/')
-            log_format = datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
-            file_logger = logging.FileHandler('logs/{}.log'.format(log_format), encoding='utf-8')
-            file_logger.setLevel(logging.DEBUG)
-            file_logger.setFormatter(formatter)
-            logger.addHandler(file_logger)
-        except OSError as e:
-            logger.exception(e)
-            raise
+        if config['log_to_files']:
+            try:
+                if not os.path.isdir(config['log_path']):
+                    os.makedirs(config['log_path'])
+                log_format = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+                file_logger = logging.FileHandler(
+                    path.join(config['log_path'], '{}.log'.format(log_format)), encoding='utf-8')
+                file_logger.setLevel(logging.DEBUG)
+                file_logger.setFormatter(formatter)
+                logger.addHandler(file_logger)
+            except OSError as e:
+                logger.exception(e)
+                raise
 
         logger_created = True
 
