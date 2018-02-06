d18a92b30f5855073e22c0a6dfa47a3f1396f10f
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-02-06 20:36:31 -0300

    Actualizar respuestas de 8ball

--- alexis/modules/8ball.py
+++ alexis/modules/8ball.py
@@ -7,8 +7,10 @@ class EightBall(Command):
     resp = ['Si', 'No', 'Quizá', 'no preguntis weas xfa', 'jaja xd', 'mira yo opino lo siguiente', 'si weon oh',
             'la respuesta a todo es jesús', 'nopo', 'nopo wn no digai esa wea', 'nuu uwu', 'sip :3',
             'la respuesta está en tu corazón', 'demah po', 'si po wn ta diciendo', 'keate piola mejor',
-            'no cacho', 'sinceramente, no sé', 'quizá', '50/50', 'eso deberías saberlo ya', 'y a mi me lo preguntas?',
-            '._.', ':thumbs_up:', ':thumbs_down:', 'no entendí', 'hable más fuerte que traigo una toalla']
+            'no cacho', 'sinceramente, no sé', 'quizá', 'eso deberías saberlo ya', 'y a mi me lo preguntas?',
+            '._.', ':thumbsup:', ':thumbsdown:', 'no entendí', 'hable más fuerte que traigo una toalla',
+            'no va a poder ser ná', 'sipo ta bn eso', 'nopo ta mal eso', 'xd', 'si pero no',
+            'esa es una muy buena pregunta']
 
     def __init__(self, bot):
         super().__init__(bot)
@@ -21,7 +23,7 @@ class EightBall(Command):
         resp = random.choice(EightBall.resp) if cmd.text != '' else 'si po jajaj XDD'
 
         embed = Embed()
-        embed.add_field(name='Pregunta:', value=':8ball: ' + text)
-        embed.add_field(name='Respuesta:', value=':speech_balloon: ' + resp)
+        embed.add_field(name='Pregunta:', value=':8ball: ' + text, inline=False)
+        embed.add_field(name='Respuesta:', value=':speech_balloon: ' + resp, inline=False)
 
         await cmd.answer(embed=embed)
