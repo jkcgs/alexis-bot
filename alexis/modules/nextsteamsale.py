5dd02e59d96a88b08cabd85c645466f574ff1eb1
Jonathan Gutierrez <contacto@jgutierrez.cl>
2018-02-08 18:36:52 -0300

    Mover variable de oferta a instancia, entre otros

--- alexis/modules/nextsteamsale.py
+++ alexis/modules/nextsteamsale.py
@@ -8,43 +8,42 @@ from alexis import Command
 
 class NextSteamSale(Command):
     url = 'https://www.whenisthenextsteamsale.com/'
-    sale = {''}
 
     def __init__(self, bot):
         super().__init__(bot)
         self.name = 'nextsteamsale'
         self.aliases = ['steamsale']
         self.help = '$[nss-help]'
+        self.sale = None
 
     async def handle(self, cmd):
-        
-        await cmd.typing()
-        sale = NextSteamSale.sale
-        if sale != '':
-            start_date = sale['StartDate'].split('T')
-            end_date = sale['EndDate'].split('T')
-            confirmed = "$[yes]" if sale['IsConfirmed'] else '$[no]'
-            remaining_time = sale['RemainingTime'].split('.')
+        if self.sale is None:
+            await cmd.answer('$[data-not-available]')
+            return
 
-            e = Embed()
-            e.description = '$[nss-title]'
-            e.add_field(name='$[name]', value=sale['Name'], inline=False)
-            e.add_field(name='$[start-date]', value='$[nss-start-date]', inline=False)
-            e.add_field(name='$[end-date]', value='$[nss-end-date]', inline=False)
-            e.add_field(name='$[nss-confirmed]', value=confirmed)
-            e.add_field(name='$[nss-length]', value='$[nss-length-value]')
-            e.add_field(name='$[nss-remaining-time]', value='$[nss-remaining-time-value]' , inline=False)
+        start_date = self.sale['StartDate'].split('T')
+        end_date = self.sale['EndDate'].split('T')
+        confirmed = "$[yes]" if self.sale['IsConfirmed'] else '$[no]'
+        remaining_time = self.sale['RemainingTime'].split('.')
 
-            await cmd.answer(embed=e, locales={
-                'startdate': start_date[0],
-                'starthour': start_date[1],
-                'enddate': end_date[0],
-                'endhour': end_date[1],
-                'days': sale['Length'],
-                'remainingdays': remaining_time[0],
-                'remaininghours': remaining_time[1]
-            })
-        
+        e = Embed()
+        e.description = '$[nss-title]'
+        e.add_field(name='$[name]', value=self.sale['Name'], inline=False)
+        e.add_field(name='$[start-date]', value='$[nss-start-date]', inline=False)
+        e.add_field(name='$[end-date]', value='$[nss-end-date]', inline=False)
+        e.add_field(name='$[nss-confirmed]', value=confirmed)
+        e.add_field(name='$[nss-length]', value='$[nss-length-value]')
+        e.add_field(name='$[nss-remaining-time]', value='$[nss-remaining-time-value]' , inline=False)
+
+        await cmd.answer(embed=e, locales={
+            'startdate': start_date[0],
+            'starthour': start_date[1],
+            'enddate': end_date[0],
+            'endhour': end_date[1],
+            'days': self.sale['Length'],
+            'remainingdays': remaining_time[0],
+            'remaininghours': remaining_time[1]
+        })
 
     async def task(self):
         await self.bot.wait_until_ready()
@@ -59,7 +58,7 @@ class NextSteamSale(Command):
             raise err
         finally:
             if not self.bot.is_closed:
-                await asyncio.sleep(15)
+                await asyncio.sleep(600)
                 self.bot.loop.create_task(self.task())
             
 
