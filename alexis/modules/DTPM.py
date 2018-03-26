fb790e7f74e7e7113661413ee17ba5d29934ded7
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-03-26 14:25:17 -0300

    Compatibilizar !ts con paraderos de un sólo recorrido

--- alexis/modules/DTPM.py
+++ alexis/modules/DTPM.py
@@ -82,15 +82,27 @@ class DTPM(Command):
             if error is not None:
                 return error.text
 
-            prox = [
-                {
-                    'route_id': p.find(id='servicio_respuesta_solo_paradero').text,
-                    'bus_plate_number': p.find(id='bus_respuesta_solo_paradero').text,
-                    'arrival_estimation': p.find(id='tiempo_respuesta_solo_paradero').text.strip(),
-                    'distance': p.find(id='distancia_respuesta_solo_paradero').text.strip()
-                }
-                for p in dom.find_all(id='proximo_solo_paradero')
-            ]
+            num_stop = dom.find(id='numero_parada_respuesta')
+            if len(num_stop.find_all(class_='texto_h2')) > 1:
+                prox = [
+                    {
+                        'route_id': list(num_stop.find_all(class_='texto_h2'))[1].text,
+                        'bus_plate_number': p.find(id='proximo_bus_respuesta').text.strip(),
+                        'arrival_estimation': p.find(id='proximo_tiempo_respuesta').text.strip(),
+                        'distance': p.find(id='proximo_distancia_respuesta').text.strip()
+                    }
+                    for p in dom.find_all(id='proximo_respuesta')
+                ]
+            else:
+                prox = [
+                    {
+                        'route_id': p.find(id='servicio_respuesta_solo_paradero').text,
+                        'bus_plate_number': p.find(id='bus_respuesta_solo_paradero').text.strip(),
+                        'arrival_estimation': p.find(id='tiempo_respuesta_solo_paradero').text.strip(),
+                        'distance': p.find(id='distancia_respuesta_solo_paradero').text.strip()
+                    }
+                    for p in dom.find_all(id='proximo_solo_paradero')
+                ]
 
             err = [
                 {
