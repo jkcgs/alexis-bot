e3fe5c4e72ffeb52b4aa4a4a9b40be6fc80361f2
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-08-05 22:00:16 -0400

    Load translations and translate furipucoin and fullwidth

--- modules/fullwidth.py
+++ modules/fullwidth.py
@@ -2,17 +2,25 @@ from bot import Command, categories
 
 
 class Fullwidth(Command):
+    __version__ = '1.0.0'
+    __author__ = 'makzk'
+    __description__ = 'Translate a set of allowed characters (below) to the fullwidth format.'
+
     supported = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()+,-./:;<=>?@[\\]^`{|}'
 
     def __init__(self, bot):
         super().__init__(bot)
         self.name = 'fullwidth'
         self.aliases = ['full']
-        self.help = 'Convierte los carácteres soportados a fullwidth y responde con el texto resultante'
+        self.help = '$[fullwidth-help]'
         self.category = categories.UTILITY
 
     async def handle(self, cmd):
-        text = [cmd.no_tags(), 'QUE WEA COXINO KLO'][int(cmd.text == '')].replace(' ', '   ')
+        # Fetch the text to convert
+        text = [cmd.no_tags(), cmd.lang.get('fullwidth-default')][int(cmd.text == '')].replace(' ', '   ')
+
+        # Translate the characters to the fullwidth format
         converted = [chr(0xFEE0 + ord(i)) if i in Fullwidth.supported else i for i in list(text)]
 
+        # Send the result and remove the author's message
         await cmd.answer_embed(''.join(converted), delete_trigger=True)
