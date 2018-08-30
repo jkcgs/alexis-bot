e0284761d0d07e3570aaa754e62d5fea9e748412
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-08-30 17:59:59 -0300

    Translations for lmgtfy, meme and pat

--- modules/pat.py
+++ modules/pat.py
@@ -18,34 +18,35 @@ class Pat(Command):
 
     async def handle(self, cmd):
         if self.config is None:
-            await cmd.answer('this is not working yet')
+            await cmd.answer('$[command-not-available]')
             return
 
+        mention = None
         if len(cmd.args) > 0:
             mention = await cmd.get_user(cmd.text)
             if mention is None:
-                await cmd.answer('usuario no encontrado. Formato: !pat [@usuario]')
+                await cmd.answer('$[user-not-found]. $[format]: $[pat-format]')
                 return
 
-            text = '{}, {} te ha dado una palmadita :3'.format(
-                mention.display_name, cmd.author_name
-            )
-
+            text = '$[pat-to-user-2]'
             if mention.id == cmd.author.id:
                 url = random.choice(self.config['self_pats'])
             elif mention.id == self.bot.user.id:
                 url = self.config['bot_pat']
-                text = 'oye nuuuu >_<'
+                text = '$[pat-to-bot]'
             else:
                 url = random.choice(self.config['pats'])
         else:
             url = random.choice(self.config['pats'])
-            text = '{}, toma una palmadita :3'.format(cmd.author_name)
+            text = '$[pat-to-user]'
 
-        await cmd.answer(embed=img_embed(url, text), withname=False)
+        await cmd.answer(embed=img_embed(url, text), withname=False, locales={
+            'user_to': mention.display_name if mention is not None else None,
+            'user_from': cmd.author.display_name
+        })
 
     async def task(self):
-        self.log.debug('[Pat] Cargando pats...')
+        self.log.debug('[Pat] Loading pats...')
         if not StaticConfig.exists('pats'):
             self.log.debug('Loading remote default pats')
             async with self.http.get(default_pats) as r:
