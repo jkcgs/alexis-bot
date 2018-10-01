6b201299d70127f1816ca2e7c2e736200e2c3f88
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-10-01 00:04:16 -0300

    Load botmessage lang files, translate invite-filter, fix $CMD replacing

--- modules/config/lang_filter.py
+++ modules/config/lang_filter.py
@@ -1,6 +1,5 @@
-from discord import Embed
-
-from bot import Command
+from bot import Command, CommandEvent
+from bot.utils import replace_everywhere
 
 
 class LangFilter(Command):
@@ -12,12 +11,22 @@ class LangFilter(Command):
         dest = kwargs.get('destination')
         svid = dest.server.id if hasattr(dest, 'server') else None
 
-        self.log.debug('prev text: %s', (kwargs.get('embed', None) or Embed()).to_dict())
-
         lang = self.get_lang(svid)
         if kwargs.get('content', '') != '':
             kwargs['content'] = lang.format(kwargs['content'], kwargs.get('locales', None))
         if kwargs.get('embed', None) is not None:
             kwargs['embed'] = lang.format(kwargs['embed'], kwargs.get('locales', None))
 
-        self.log.debug('post text: %s', (kwargs.get('embed', None) or Embed()).to_dict())
+        if 'event' in kwargs:
+            kwargs['content'] = kwargs['content'].replace('$CMD', '$PX$NM')
+            kwargs['content'] = kwargs['content'].replace('$PX', kwargs['event'].prefix)
+
+            if kwargs['embed'] is not None:
+                replace_everywhere(kwargs['embed'], '$CMD', '$PX$NM')
+                replace_everywhere(kwargs['embed'], '$PX', kwargs['event'].prefix)
+
+            if isinstance(kwargs['event'], CommandEvent):
+                kwargs['content'] = kwargs['content'].replace('$NM', kwargs['event'].cmdname)
+
+                if kwargs['embed'] is not None:
+                    replace_everywhere(kwargs['embed'], '$NM', kwargs['event'].cmdname)
