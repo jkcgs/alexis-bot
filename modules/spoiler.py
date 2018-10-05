50f6517eb39253b0a68d1d9525ca2f612ccdf781
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-10-04 23:28:18 -0300

    !spoiler

---
+++ modules/spoiler.py
@@ -0,0 +1,42 @@
+import base64
+
+import discord
+from discord import Embed
+
+from bot import Command
+
+url = 'https://assets.imgix.net/examples/clouds.jpg?w=640&txtsize=64&txtfont64=RnV0dXJhIENvbmRlbnNlZCBNZWRpdW0' \
+      '&txtclr=ff2e4357&txtalign=middle%2Ccenter&txt64={}&h=200&fit=crop&blur=150&txtfit=max'
+
+
+class Spoiler(Command):
+    __author__ = 'makzk'
+    __version__ = '0.0.1'
+
+    def __init__(self, bot):
+        super().__init__(bot)
+        self.name = 'spoiler'
+        self.help = '$[spoiler-help]'
+        self.allow_pm = False
+
+    async def handle(self, cmd):
+
+        if not self.can_delete_msg(cmd.server):
+            await cmd.answer('$[spoiler-error-cant]', locales={'mention': cmd.author.mention})
+        else:
+            try:
+                await self.bot.delete_message(cmd.message)
+            except discord.Forbidden:
+                await cmd.answer('$[spoiler-error-couldnt]', locales={'mention': cmd.author.mention})
+
+        if cmd.argc == 0:
+            await cmd.answer('$[format]: $[spoiler-format]')
+            return
+
+        enc = base64.b64encode(cmd.text.encode('utf-8'))
+        img_url = url.format(enc.decode('utf-8'))
+        await cmd.answer(Embed(description='[$[spoiler-link]]({})'.format(img_url)))
+
+    def can_delete_msg(self, server):
+        self_member = server.get_member(self.bot.user.id)
+        return self_member.server_permissions.manage_messages
