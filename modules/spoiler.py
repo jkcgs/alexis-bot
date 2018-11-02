9bc8cc889058940a5fd5b399d0f8bfe7b990f825
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-11-01 21:02:32 -0300

    Add title argument to !spoiler, move requests to 2.20.0

--- modules/spoiler.py
+++ modules/spoiler.py
@@ -11,7 +11,7 @@ url = 'https://assets.imgix.net/examples/clouds.jpg?w=640&txtsize=64&txtfont64=R
 
 class Spoiler(Command):
     __author__ = 'makzk'
-    __version__ = '1.0.0'
+    __version__ = '1.1.0'
 
     def __init__(self, bot):
         super().__init__(bot)
@@ -21,6 +21,9 @@ class Spoiler(Command):
         self.allow_pm = False
 
     async def handle(self, cmd):
+        if cmd.argc == 0:
+            await cmd.answer('$[format]: $[spoiler-format]')
+            return
 
         if not self.can_delete_msg(cmd.server):
             await cmd.answer('$[spoiler-error-cant]', locales={'mention': cmd.author.mention})
@@ -31,15 +34,19 @@ class Spoiler(Command):
             except discord.Forbidden:
                 await cmd.answer('$[spoiler-error-couldnt]', locales={'mention': cmd.author.mention})
 
-        if cmd.argc == 0:
-            await cmd.answer('$[format]: $[spoiler-format]')
-            return
+        args = cmd.text.split('|')
 
-        enc = base64.b64encode(cmd.text.encode('utf-8'))
+        enc = base64.b64encode(args[0].encode('utf-8'))
         img_url = url.format(enc.decode('utf-8'))
         emb = Embed(description='[$[spoiler-link]]({})'.format(img_url))
         emb.set_footer(text='$[spoiler-from]', icon_url=cmd.author.avatar_url or cmd.author.default_avatar_url)
-        await cmd.answer(emb)
+
+        locales = {}
+        if len(args) > 1:
+            emb.title = '$[spoiler-title]'
+            locales['title'] = args[1]
+
+        await cmd.answer(emb, locales=locales)
 
     def can_delete_msg(self, server):
         self_member = server.get_member(self.bot.user.id)
