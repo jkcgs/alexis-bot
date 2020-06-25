e192edb3021f145637d82e218d8a5de5065027b1
makzk <contacto@jgutierrez.cl>
2020-06-24 23:29:14 -0400

    covid19cl telegram??????????????

--- modules/covid19cl.py
+++ modules/covid19cl.py
@@ -1,8 +1,12 @@
+import asyncio
+import json
 from datetime import datetime
+from json import JSONDecodeError
 
 from discord import Embed
 
 from bot import Command, categories
+from bot.guild_configuration import GuildConfiguration
 
 cached_data = None
 cached_ts = datetime.now().timestamp()
@@ -10,25 +14,40 @@ last_date = None
 
 
 def nf(val):
-    return '.'.join([str(val)[::-1][i:i + 3] for i in range(0, len(str(val)), 3)])[::-1]
+    return '.'.join([str(val)[::-1][i:i + 3] for i in range(0, len(str(val)), 3)])[::-1].replace('-.', '-')
 
 
-def embed(data, data_diff):
-    description = \
-        f'Información del día **{data["fecha"]}** (hasta las 21:00 del día anterior)\n\n' \
-        f'**Total confirmados:** {data_diff["confirmados"]}\n' \
+def data_diff(data):
+    diff = {}
+    fields = ['activos', 'conectados', 'confirmados', 'criticos', 'fallecidos',
+              'recuperados', 'ventiladores_disp']
+    for field in fields:
+        diff[field] = nf(data[field])
+        if data['ayer']:
+            val = data[field] - data['ayer'][field]
+            diff[field] += ' ({})'.format(['', '+'][int(val > 0)] + nf(val))
+    return diff
+
+
+def description(data):
+    diff = data_diff(data)
+    return f'Información del día **{data["fecha"]}** (hasta las 21:00 del día anterior)\n\n' \
+        f'**Total confirmados:** {diff["confirmados"]}\n' \
         f'*({nf(data["sintomaticos"])} sintomáticos, {nf(data["asintomaticos"])} asintomáticos, ' \
         f'{nf(data["sin_notificar"])} sin notificar)*\n' \
-        f'**Total activos:** {data_diff["activos"]}\n' \
-        f'**Recuperados:** {data_diff["recuperados"]}\n' \
-        f'**Fallecidos:** {data_diff["fallecidos"]}\n\n' \
+        f'**Total activos:** {diff["activos"]}\n' \
+        f'**Recuperados:** {diff["recuperados"]}\n' \
+        f'**Fallecidos:** {diff["fallecidos"]}\n\n' \
         f'**Exámenes realizados:** {nf(data["total_examenes"])} (+{nf(data["examenes"])})\n' \
-        f'**Pacientes conectados:** {data_diff["conectados"]}, críticos: {data_diff["criticos"]}\n' \
-        f'**Ventiladores disponibles:** {data_diff["ventiladores_disp"]}\n\n' \
+        f'**Pacientes conectados:** {diff["conectados"]}, críticos: {diff["criticos"]}\n' \
+        f'**Ventiladores disponibles:** {diff["ventiladores_disp"]}\n\n' \
         f'**Residencias sanitarias**: {data["rs_residencias"]} ' \
         f'con {nf(data["rs_habitaciones"])} habitaciones'
 
-    the_embed = Embed(title='Estado del Coronavirus COVID-19 en Chile', description=description)
+
+def embed(data):
+    desc = description(data)
+    the_embed = Embed(title='Estado del Coronavirus COVID-19 en Chile', description=desc)
     the_embed.set_footer(text='Información actualizada al: {}'.format(data['ts_capturado']))
     return the_embed
 
@@ -37,50 +56,72 @@ class Covid19CL(Command):
     __author__ = 'makzk'
     __version__ = '1.0.1'
     url = 'https://api.mak.wtf/covid'
+    _last_day = None
 
     def __init__(self, bot):
         super().__init__(bot)
         self.name = 'covid19cl'
         self.aliases = ['covid', 'coronavirus']
         self.category = categories.INFORMATION
+        self.config = GuildConfiguration.get_instance()
+
+        self.schedule = (self.task, 60)
+
+        self.default_config = {
+            'covid19cl_telegram_key': '',
+            'covid19cl_telegram_channel': ''
+        }
 
     async def handle(self, cmd):
         try:
-            await cmd.typing()
-            data = await self.get_data()
+            data = json.loads(self.config.get('covid19cl_data', '{}', create=False))
+            if not data:
+                await cmd.answer('La información aún no está disponible.')
+                return
 
-            data_diff = {}
-            fields = ['activos', 'conectados', 'confirmados', 'criticos', 'fallecidos',
-                      'recuperados', 'ventiladores_disp']
-            for field in fields:
-                data_diff[field] = nf(data[field])
-                if data['ayer']:
-                    val = data[field] - data['ayer'][field]
-                    data_diff[field] += ' ({})'.format(['', '+'][int(val > 0)] + nf(val))
-
-            the_embed = embed(data, data_diff)
+            the_embed = embed(data)
             await cmd.answer(embed=the_embed)
-
             return
         except Exception as e:
             self.log.error(e)
             await cmd.answer('No se pudo cargar la información: {}'.format(str(e)))
 
-    async def get_data(self):
-        global cached_data, cached_ts, last_date
+    async def publish(self, data):
+        tg_key = self.bot.config.get('covid19cl_telegram_key')
+        tg_api = 'https://api.telegram.org/bot{apikey}/sendMessage'.format(apikey=tg_key)
+        tg_chanid = self.bot.config.get('covid19cl_telegram_channel')
+
+        message = description(data)
+        the_embed = embed(data)
+
+        self.log.debug('Publishing Covid19 data to Discord...')
+        chan = self.bot.get_channel(int(self.bot.config.get('covid19cl_discord_channel')))
+        await self.bot.send_message(chan, embed=the_embed)
+
+        self.log.debug('Publishing Covid19 data to Telegram...')
+        tg_message = message.replace('**', '#').replace('*', '_').replace('#', '*') + '\n\nSent by AlexisBot™'
+        tg_data = {'chat_id': tg_chanid, 'text': tg_message, 'parse_mode': 'Markdown'}
+        await self.http.post(tg_api, json=tg_data)
+
+    async def task(self):
         now = datetime.now()
-        now_date = now.strftime('%m%d')
-        if last_date and cached_data and (last_date == now_date or (cached_ts + 10) > datetime.now().timestamp()):
-            return cached_data
+        curr_data = json.loads(self.config.get('covid19cl_data', '{}', create=False))
 
-        self.log.debug('Updating data...')
-        async with self.http.get(self.url) as r:
-            if r.status != 200:
-                raise ValueError('No se pudo obtener la información ({}).'.format(r.status))
+        if not curr_data or (self._last_day != now.day and now.hour >= 10):
+            try:
+                self.log.debug('Loading Covid19 data...')
+                async with self.http.get(self.url) as r:
+                    if r.status != 200:
+                        raise RuntimeError('status ' + str(r.status))
+                    data = await r.json()
+                    if 'ts_capturado' not in data:
+                        raise RuntimeError('invalid data')
 
-            data = await r.json()
-            cached_data = data
-            now = datetime.now()
-            cached_ts = now.timestamp()
-            last_date = now.strftime('%m%d')
-            return data
+                    if curr_data.get('fecha', '') != data['fecha'] and data['listo']:
+                        self.config.set('covid19cl_data', json.dumps(data))
+                        _last_day = now.day
+                        if curr_data is not None:
+                            self.log.debug('Publishing Covid19 data...')
+                            await self.publish(data)
+            except (JSONDecodeError, RuntimeError) as e:
+                self.log.warning('No se pudo obtener la información ({}).'.format(e))
